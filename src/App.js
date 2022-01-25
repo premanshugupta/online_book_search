685c3647b7449685b8856d53b9d7478840d370a5
premanshugupta <premanshu20032001@gmail.com>
2022-01-25 19:56:48 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Form, FormControl, FormGroup, FormLabel,Container} from 'react-bootstrap';
+import {Button, Form, FormControl, FormGroup, FormLabel,Container,Row,Col,Card} from 'react-bootstrap';
 import { useState } from 'react';
 
 function App() {
@@ -36,12 +36,48 @@ function App() {
           <Button  variant='success' type='submit'>Search</Button>
         </Form>
         </Container>
+        <Container>
+                <Row>
+                    <Col lg="12">
+                                       
+                        {result.map((book,key)=>{
+                            return (
+                                <div>
+                                <Card>
+                        <Card.Body>
+                            <Row>
+                                <Col lg="4" style={{textAlign:'center'}}>
+                                    <>
+                                    <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/><br/><br/>
+                                    </>
+                                <h5 style={{color:'#000'}}>{book.volumeInfo.title}</h5>
+                                </Col>
+                                <Col lg="8">
+                                <h5 style={{color:'#000'}}>{book.volumeInfo.description}</h5><br/><br/><br/>
+                                <a target="_blank" href={book.volumeInfo.previewLink}>
+                                <Button  variant='success' type='submit'>OPEN BOOK</Button>
+                                </a>
+                                               
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                        </Card>
+                        <br/>
+                        </div>
+                            )
+                        })}
+                        
+                    </Col>
+                </Row>
+            </Container>
         {result.map(book => (
-
+          <Container>
           <a target="_blank" href={book.volumeInfo.previewLink}>
 
           <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
           </a>
+          <Form.Text className='text-muted'>{book.volumeInfo.title}</Form.Text>
+          </Container>
         ))}
       </header>
     </div>
